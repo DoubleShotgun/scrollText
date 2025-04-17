86dea3e2a7a3448087bae9be3c5c93ae96f57b80
DoubleShotgun <tminecraftj@gmail.com>
2025-04-17 21:23:05 +1000

    Made it better lol

--- scrollText.py
+++ scrollText.py
@@ -1,19 +1,28 @@
 class scrollText:
-  def __init__(self,fullText,skip,capacity):
-    self.fullText = fullText
-    self.skip     = skip
-    self.capacity = capacity
-    self.text     = fullText[:capacity]
-    self.count    = 0
-    
-  def step(self):
-    self.count += self.skip
-    self.count %= len(self.fullText)+1
-    self.text = self.fullText[self.count:self.capacity+self.count]
-    
-    if self.count+self.capacity >= len(self.fullText)+1:
-      self.text += " "+self.fullText[:self.capacity-len(self.text)-1]
-      
-  def __str__(self):
-    self.step()
-    return self.text
+    def __init__(self,fullText,skip,capacity):
+        self.fullText = fullText
+        self.skip     = skip
+        self.capacity = capacity
+        self.text     = fullText[:capacity]
+        self.offset   = 0
+
+    def step(self):
+        if len(self.fullText) <= self.capacity:
+            self.text = self.fullText
+            return
+            
+        self.offset += self.skip
+        self.offset %= len(self.fullText) + 1
+        self.text = self.fullText[self.offset:self.capacity + self.offset]
+
+        if self.offset+self.capacity >= len(self.fullText)+1:
+            self.text += " "+self.fullText[:self.capacity-len(self.text)-1]
+
+    def setString(self,fullText):
+        self.fullText = fullText
+        self.offset = 0
+        self.text = fullText[:self.capacity]
+
+    def __str__(self):
+        self.step()
+        return self.text
